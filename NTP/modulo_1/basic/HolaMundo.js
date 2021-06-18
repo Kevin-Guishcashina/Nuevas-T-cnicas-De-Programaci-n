aed89c3d25ea68b3a6a8fa3b2df13675599dfbd0
Kevin-Guishcashina <kevinguishcashinaxd@gamil.com>
2021-06-18 17:43:31 -0500

    Tarea
    Deber de funciones de console

--- NTP/modulo_1/basic/HolaMundo.js
+++ NTP/modulo_1/basic/HolaMundo.js
@@ -1,2 +1,13 @@
 console.log("Hola mundo");
+// TAREA
 //CONSULTAR LAS FUNCIONES DE CONSOLE.
+
+console.log("Kevin Guishcashina");
+
+console.warn("Este es mi perfil");
+
+console.error("Este es un mensae de error");
+
+console.info("Este es un mensaje de informacion");
+
+console.debug("Este es un mensaje de debug");
