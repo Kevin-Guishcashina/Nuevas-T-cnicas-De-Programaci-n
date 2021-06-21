e4a9482c662354cff506445261cc5a4ca53601b0
Kevin-Guishcashina <kevinguishcashinaxd@gamil.com>
2021-06-21 12:03:56 -0500

    Codigo
    Código de Funciones y arreglos

---
+++ NTP/modulo_1/basic/07_Arreglos.js
@@ -0,0 +1,45 @@
+/*
+ARREGLOS
+*/
+//CONCAT
+var numeros = [1,2,3,4,5];
+var nuevoNumeros = numeros.concat([6,7,8,9,0])
+//console.log(numeros);
+//console.log(nuevoNumeros)
+
+//  JOIN
+var nombre = ["k","e","v","i","n"]
+//console.log(nombre.join(""));
+
+//   SLICE
+//console.log(nombre.slice(2));
+
+//   INDEX OF
+//console.log(nombre.indexOf("e"));
+
+//  LAST INDEX OF
+//console.log(nombre.lastIndexOf("e"));
+
+//   REVERSE
+//console.log(nombre.reverse());
+
+//    SORT
+var numerosDesorenados = [4,8,5,6,3,8,5,9];
+//console.log(numerosDesorenados.sort());
+
+//   SHIFT
+//console.log(numerosDesorenados.shift());
+
+//   POP
+//console.log(numerosDesorenados.pop());
+
+//   PUSH
+numerosDesorenados.push(1012);
+//console.log(numerosDesorenados);
+
+var pares = [2,4,6,8,10];
+console.log(pares.map((elemento) => elemento+1));
+
+// DEBER CONSULTAR FUNCIONES DE ARREGLOS
+
+// 
