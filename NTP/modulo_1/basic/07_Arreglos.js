8d16440855976d217ea296052e84ad3fd0d0415c
Kevin-Guishcashina <kevinguishcashinaxd@gamil.com>
2021-06-21 16:31:39 -0500

    Tarea
    Consulta de funciones de arreglos

--- NTP/modulo_1/basic/07_Arreglos.js
+++ NTP/modulo_1/basic/07_Arreglos.js
@@ -38,8 +38,39 @@ numerosDesorenados.push(1012);
 //console.log(numerosDesorenados);
 
 var pares = [2,4,6,8,10];
-console.log(pares.map((elemento) => elemento+1));
+//console.log(pares.map((elemento) => elemento+1));
 
-// DEBER CONSULTAR FUNCIONES DE ARREGLOS
+//                DEBER CONSULTAR FUNCIONES DE ARREGLOS
 
-// 
+// 1) INCLUDES
+/*
+var valores = [1,2,3];
+console.log(valores.includes(2));
+*/
+
+// 2) FILTER
+/*
+var valores = [1,2,3,4,5]
+var valores2 = valores.filter(num => num > 3 );
+console.log(valores2);
+console.log(valores);
+*/
+
+// 3) REDUCE
+/*
+var valores = [1,2,3,4,5,6];
+var suma = valores.reduce((total, value) => total + value, 0 );
+console.log(suma)
+*/
+
+// 4) DELETE
+/*
+var valores = [1,2,3,4,5,6];
+console.log('Hay: '+valores.length+ ' valores');
+*/
+
+// 5) UNSHIFT
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril"];
+meses.unshift("Primer elemento");
+console.log(meses);
