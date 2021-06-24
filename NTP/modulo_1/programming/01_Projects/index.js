8e3579da9cd9cccf5a898dc6e92fac104c785f6a
Kevin-Guishcashina <kevinguishcashinaxd@gamil.com>
2021-06-24 12:09:13 -0500

    Clase query, params, router
    Codigo de clase, implementaciones y creacion de router

--- NTP/modulo_1/programming/01_Projects/index.js
+++ NTP/modulo_1/programming/01_Projects/index.js
@@ -11,6 +11,36 @@ app.get('/', (req, res) => {
     res.send("Hola mundo ExpressJs");
 });
 
+app.get("/saludo", (req, res) => {
+    // rep.query
+    //console.log(req.query);
+    //obtenemos la query del objeto request
+    /*
+    //otra forma
+    
+    primera forma
+    const { 
+        query: { nombre, apellido }, 
+    } = req;*/
+const {query} = req;
+    //segunda forma
+    //const nombre = req.query.nombre
+    //const apellido = req.query.apellido
+
+    res.json({
+        saludo: `Hola soy ${query.nombre} ${query.apellido}`,
+    });
+});
+
+//para parametro con /:
+app.get('/saludo/:nombre', (req, res)=>{
+    const { params: { nombre } } = req
+    //const nombre = req.params.nombre
+    //console.log(req.params);
+    res.json({
+        nombre: nombre
+    })
+})
 app.listen(3000, () => {
     console.log("Servicodr escuchando en http://localhost:3000");
 })
---
+++ NTP/modulo_1/programming/01_Projects/routes/index.js
@@ -0,0 +1,19 @@
+//CREACION DEL ROUTER
+//traemos el paquete
+const { Router } = require('express');
+//Inicializamos Router
+const router = Router()
+
+//Creacion e los endpoint
+// Exportamos las rutas
+module.exports.RouterIndex = router;
+//module.exports.OtraCosa = "Kevin";
+
+
+/*
+module = {
+    exports: {
+        RouterIndex: router
+    }
+}
+*/
