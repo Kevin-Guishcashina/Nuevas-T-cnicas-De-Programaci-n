f418362ad7ae6620a2d941aaf36c23e8ccdc68f4
Kevin-Guishcashina <kevinguishcashinaxd@gamil.com>
2021-07-02 11:58:19 -0500

    Codigo
    codigo

--- NTP/modulo_1/programming/02_Implementations/01_Clients/lib/Mongo.js
+++ NTP/modulo_1/programming/02_Implementations/01_Clients/lib/Mongo.js
@@ -2,8 +2,9 @@
 const { MongoClient } = require('mongodb');
 
 const DB_NAME = "clients";
-const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.wjh0q.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
+//const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.wjh0q.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@proyecto.wjh0q.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+console.log(URL);
 var MongoConnection = () => new Promise(async (resolve, reject) => {
     try {
         let client = new MongoClient(URL, {
--- NTP/modulo_1/programming/02_Implementations/01_Clients/services/Clients.service.js
+++ NTP/modulo_1/programming/02_Implementations/01_Clients/services/Clients.service.js
@@ -8,15 +8,27 @@ const findUsers = () => new Promise (async (resolve, reject) => {
       //inicializa mongoclient para que retorne la configuracion de la db
         const db = await MongoConnection();
       //obtenemos la collection
-        const clients = DB.collection(COLLECTION); 
+        const clients = db.collection(COLLECTION); 
         const clientsList = await clients.find({}).toArray();
         resolve(clientsList);
     } catch (error) {
         reject(error)
-    }
-    
+    }  
+});
+
+//FUNCION PARA INGRESO DE DATOS
+const createUser = (user) => new Promise (async (resolve, reject) => {
+  try {
+    const db = await MongoConnection();
+    const clients = db.collection(COLLECTION);
+    const result = await clients.insertOne(user);
+    resolve(result);
+  } catch (error) {
+    reject(error)
+  }
 })
 
 module.exports = {
     findUsers,
+    createUser,
 }
